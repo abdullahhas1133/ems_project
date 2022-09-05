d0d31acb3d1a8c808a1e0dd410f69d4cb343daa8
abdullah126 <abdullah.has126@gmail.com>
2022-09-05 22:39:22 +0500

    permitted additional paramters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: %i[email user_name first_name last_name encrypted_password])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[email user_name first_name last_name encrypted_password])
+  end
 end

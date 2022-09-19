b9adb56be1080e645611e7e632ef9f5d3304c3b2
abdullah126 <abdullah.has126@gmail.com>
2022-09-19 16:55:06 +0500

    Reorganising Routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,15 @@
 # Application_controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+ 
   private
 
   def current_cart
     @current_cart = current_user.cart
   end
 
-  def after_sign_in_path(_resoucrce)
+  def after_sign_in_path_for(resource)
     current_user.admin? ? admin_users_path : user_products_path
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # default_url_options host: 'localhost:3000'
   root 'admin/users#index'
   devise_for :users, controllers: { registrations: 'registrations' }
 
@@ -20,11 +19,5 @@ Rails.application.routes.draw do
     resources :orders
   end
 
-  post 'order_items/:id/add' => 'order_items#add_quantity', as: 'order_item_add'
-  post 'order_items/:id/reduce' => 'order_items#reduce_quantity', as: 'order_item_reduce'
-
-  post 'user/orders/coupon' => 'user/orders#coupon', as: 'orders_coupon'
-
   resources :invitations, only: %i[new create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

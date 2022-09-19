a59a25d6d4ac2bca89accc762279702ae72fbe81
abdullah126 <abdullah.has126@gmail.com>
2022-09-19 17:13:07 +0500

    Adding Model Validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,5 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   enum payment: %i[cash card]
+  validate :first_name, :last_name, :email, :address, presense: true
 end

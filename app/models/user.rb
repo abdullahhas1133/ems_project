df51e75cdc5af286e99e3f8893b7c5330d83f9e7
abdullah126 <abdullah.has126@gmail.com>
2022-09-29 17:58:34 +0500

    User Order Delete Issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   attr_writer :login
 
-  has_many :orders, dependent: :destroy
+  has_many :orders, dependent: :nullify
   has_one :cart
   enum role: %i[client admin]
 
